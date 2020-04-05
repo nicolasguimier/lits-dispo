20c8f2bef97023a36ebee6fd022f6ecee4a87b42
Nicolas Guimier <nicolas.g@flexishore.com>
2020-04-05 08:56:42 +0200

    add text filter on hospital listing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,26 @@ import { initAutocomplete } from '../plugins/init_autocomplete.js';
 
 initMapbox();
 initAutocomplete();
+
+// Search hospital in the listing.
+
+const searchHospitalElement = document.getElementById("searchHospitalInput");
+if (searchHospitalElement) {
+	searchHospitalElement.addEventListener("keyup", function(){
+	  let hospitalElement, i, txtValue;
+	  const filter = searchHospitalElement.value.toUpperCase();
+	  const itemListing = document.querySelectorAll('.hospital-card');
+
+	  // Loop through all list items, and hide those who don't match the search query
+	  for (i = 0; i < itemListing.length; i++) {
+	    hospitalElement = itemListing[i].querySelectorAll(".hospital-text")[0];
+	    txtValue = hospitalElement.textContent || hospitalElement.innerText;
+	    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+	      itemListing[i].style.display = "";
+	    } else {
+	      itemListing[i].style.display = "none";
+	    }
+	  }
+
+	});
+}
