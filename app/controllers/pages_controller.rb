089df59135a8e914a8059c7d3082b5c9f86fa744
Nicolas Guimier <nicolas.g@flexishore.com>
2020-04-10 18:07:46 +0200

    footer and pages for footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about, :condition]
+  skip_before_action :authenticate_user!, only: [:home, :about, :condition, :mentions]
 
   def home
   end
@@ -10,4 +10,6 @@ class PagesController < ApplicationController
   def condition
   end
 
+  def mentions
+  end
 end
