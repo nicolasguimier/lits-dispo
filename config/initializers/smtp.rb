6dec0ea3977e3bda5ad8bf7a33efcb8be899d3de
Nicolas Guimier <nicolas.g@flexishore.com>
2020-04-17 15:00:00 +0200

    Add contact form

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,6 @@ ActionMailer::Base.smtp_settings = {
   address: "ssl0.ovh.net",
   port: '587',
   # tls: true,
-  # domain: 'swatchx007moviemarathon.fr',
   user_name: ENV['SMTP_ADDRESS'],
   password: ENV['SMTP_PASSWORD'],
   authentication: :login,
