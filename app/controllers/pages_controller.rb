d8c5f4857d57fb0aae2188b26dd54f03a7968be0
Nicolas Guimier <nicolas.g@flexishore.com>
2020-04-17 15:01:33 +0200

    clean Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about, :condition, :mentions, :contact]
+  skip_before_action :authenticate_user!, only: [:home, :about, :condition, :mentions]
 
   def home
   end
